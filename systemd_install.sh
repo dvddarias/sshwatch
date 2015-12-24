d7fdcba2991c3d5899b725bf72966a8f9a811599
David Darias <dvd.darias@gmail.com>
2015-12-23 21:37:33 -0500

    Improved the systemd install script

--- systemd_install.sh
+++ systemd_install.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+#---------------------------------EDIT HERE-----------------------------------------
 name="sshwatch"
 shot_desc="SSH config swapper."
 desc="A daemon to swap the ~/.ssh/config file in correspondence with the network profile."
 
 user="$(whoami)" #the user name that will run the script
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get the bash script directory
-
 script_path="$dir/$name"
-run_path="/usr/local/bin/$name"
+#Uncomment this line to set the path of a configuration file
+#script_config="$dir/config.json"
+
 daemon_pwd="/usr/local/bin"
-opts="none"
+daemon_path="/usr/local/bin/$name"
+daemon_config="/etc/$name"
+daemon_opts=" "
+#-----------------END OF EDIT UNLESS YOU KNOW WHAT YOU ARE DOING--------------------
 
 if [[ $# -eq 1 && "$1" = "-u" ]]; then
-	sudo systemctl stop "$name"
-	sudo rm "/etc/systemd/${name}.service"
-	sudo rm "${run_path}/${name}"
-	exit
+    sudo systemctl stop "${name}.service"
+    sudo systemctl disable "${name}.service"
+    sudo rm "$daemon_path"
+    sudo rm -rf "$daemon_config"
+    sudo rm "/etc/systemd/system/${name}.service"
+    exit
 fi
 
 service="[Unit]
@@ -27,7 +34,7 @@ After=network.target
 Type=simple
 User=$user
 WorkingDirectory=$daemon_pwd
-ExecStart=${run_path} $opts
+ExecStart=$daemon_path $daemon_opts
 Restart=always
 
 [Install]
@@ -37,9 +44,16 @@ WantedBy=multi-user.target
 printf "$service" > "/tmp/${name}.service"
 
 #copy the executable to local/bin
-sudo cp "$script_path" "${run_path}"
-sudo chmod +x "${run_path}"
+sudo cp "$script_path" "$daemon_path"
+sudo chmod +x "$daemon_path"
+
+#copy the configuration if it is declared
+sudo mkdir -p "$daemon_config"
+if [[ -n "${script_config}" ]]; then
+    sudo cp "$script_config" "$daemon_config/${name}.config"
+fi
 
 #install the systemd service
-sudo mv "/tmp/${name}.service" "/etc/systemd/${name}.service"
-sudo systemctl start "$name"
+sudo mv "/tmp/${name}.service" "/etc/systemd/system/${name}.service"
+sudo systemctl enable "${name}.service"
+sudo systemctl start "${name}.service"
